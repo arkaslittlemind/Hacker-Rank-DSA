ff809474974e4c9621a9041fce6837894ea0f437
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-07-23 01:25:33 +0530

    Create Pointers.cpp

---
+++ C++/Pointers.cpp
@@ -0,0 +1,22 @@
+https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?h_r=next-challenge&h_v=zen
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int sum = *a + *b;
+    int absDifference = *a - *b > 0 ? *a - *b : -(*a - *b);
+    *a = sum;
+    *b = absDifference;   
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
